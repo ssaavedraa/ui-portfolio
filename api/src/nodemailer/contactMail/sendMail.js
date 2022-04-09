49181fe9ec3b5f9d898fca4f5a00013eea6f6beb
ssaavedraa <santiagosaavedra98@gmail.com>
2022-04-09 01:23:25 -0500

    minor fixes email

--- api/src/nodemailer/contactMail/sendMail.js
+++ api/src/nodemailer/contactMail/sendMail.js
@@ -11,6 +11,8 @@ const emailSender = async (sender, fullName, message, phone) => {
         text: `${message} \n Reply to email: ${sender} \n Reply to WhatsApp: ${phone}`
     }
 
+    console.log(mailOptions.text)
+
     await transporter.sendMail(mailOptions, (error, info) => {
         if (error){
             console.log(error)
