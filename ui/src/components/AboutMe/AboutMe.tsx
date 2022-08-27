60f7bf72fb9de66c4d84c1ebdae0ba2a2078f9b0
ssaavedraa <=>
2022-08-27 11:47:57 -0500

    changed about me design

--- ui/src/components/AboutMe/AboutMe.tsx
+++ ui/src/components/AboutMe/AboutMe.tsx
@@ -8,49 +8,14 @@ export default function AboutMe() {
   return (
     <div className="about-me">
       <img className='about-me__thumbnail' src={profilePicture} alt="santiago saavedra profile" />
-      <div className='test'>
-        <code className='about-me__profile'>{
-          `const profile = {\n
-          name: Santiago,\n
-          lastNames: Saavedra Arevalo,\n
-          occupation: Fullstack Developer,\n
-          skills: {\n
-            frontend: [\n
-              react: {\n
-                ssr: false,\n
-                javascript: true,\n
-                typescript: false\n
-                relatedJobs: undefined\n
-              },\n
-              vue: {\n
-                ssr: true,\n
-                javascript: true,\n
-                typescript: false\n
-                realtedJobs: [\n
-                  softwareDeveloper: 'Torre Labs'\n
-                ]\n
-              }\n
-            ]\n
-            backend: [\n
-              playFramework: {\n
-                language: 'Scala',\n
-                relatedJobs: [\n
-                  softwareDeveloper: 'Torre Labs'\n
-                ]\n
-              },\n
-              springboot: {\n
-                language: 'Java',\n
-                relatedJobs: undefined\n
-              },\n
-              express: {\n
-                janguage:'JavaScript',\n
-                relatedJobs: undefined\n
-              }\n
-            ]\n
-          }\n
-        }`
-        }
-        </code>
+      <div className="about-me__header">
+        <h1 className='about-me__header--title'>
+          Fullstack Web Developer
+        </h1>
+        <div className='about-me__button-container'>
+          <button className='btn btn--primary'>Contact me</button>
+          <button className='btn btn--primary'>Download CV</button>
+        </div>
       </div>
     </div>
   );
