bd3796ce693e4d58daa9197133ddc5c1e6bd12ef
ssaavedraa <=>
2022-09-02 01:28:05 -0500

    api migration to typescript

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./src/app.js')
-
-const port = process.env.PORT || 3001
-
-server.listen(port, () => {
-    console.log(`listening at port ${port}`)
-})
--- api/src/routes/emailRoute.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-
-const router = Router()
-const emailController = require('../controllers/email/index')
-
-router.post('/send', emailController.send)
-
-module.exports = router
