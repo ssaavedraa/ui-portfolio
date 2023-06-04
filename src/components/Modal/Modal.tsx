4a15a7ead6949db58f6ea0133d6cc4a5120e70cc
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-06-03 20:44:11 -0500

    fix modal styling for mobile

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -16,7 +16,7 @@ export default function Modal ({ isOpen, showCloseOption, onClose, children }: M
 
   return ReactDOM.createPortal(
     <div className='w-screen h-screen bg-[#23233286] fixed top-0 overflow-x-hidden z-[50000] flex items-center'>
-      <div className='m-auto bg-slate-600 w-1/3 relative rounded-lg h-1/3 shadow-2xl'>
+      <div className='m-auto bg-slate-600 w-full md:w-1/3 relative rounded-lg h-1/3 shadow-2xl'>
         {(showCloseOption === true) && <div className='relative h-[12%] border-b-2'>
           <p className='text-4xl absolute right-2'>
             &times;
