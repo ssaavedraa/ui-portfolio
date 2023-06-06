b4450ed7ce1f30acd4333e5e40d9dc46d0aca173
ssaavedraa <=>
2023-06-05 19:23:54 -0500

    fetch project from api

--- src/components/Modal/Error/Error.tsx
+++ src/components/Modal/Error/Error.tsx
@@ -2,7 +2,7 @@ import { RxCrossCircled } from 'react-icons/rx'
 
 export default function Error ({ message }: { message: string }): JSX.Element {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center'>
       <RxCrossCircled className='h-[100px] w-[100px] m-auto text-red-500'/>
       <span className='mt-2 block'>{message}</span>
     </div>
--- src/components/Modal/Loader/Loader.tsx
+++ src/components/Modal/Loader/Loader.tsx
@@ -1,8 +1,8 @@
 export default function Loader ({ message }: { message: string }): JSX.Element {
   return (
-    <div>
+    <div className='flex-col flex items-center justify-center'>
       <div className='h-[100px] w-[100px] border-8 rounded-full border-[#ffffff67] border-t-blue-600 animate-spin m-auto'/>
-      <span className='mt-2 block'>{message}</span>
+      <span className='mt-2 block text-2xl'>{message}</span>
     </div>
   )
 }
