1291d9f6d9da46d39c5f26103adea9500d3c25a3
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-06-13 01:21:02 -0500

    remove unused components

--- components/ScrollDown/ScrollDown.tsx
+++
@@ -1,9 +0,0 @@
-import './ScrollDown.css'
-
-export function ScrollDown (): JSX.Element {
-  return (
-    <div className='scrolldown'>
-      <p><i className='bx bx-chevron-down'></i></p>
-    </div>
-  )
-}
