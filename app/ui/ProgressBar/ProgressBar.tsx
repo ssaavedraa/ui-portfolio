efb7c620f3bc78ba66f01ca50550f068315ccc34
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-06-13 15:23:04 -0500

    add projects page

---
+++ app/ui/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,22 @@
+import utilStyles from '../../../styles/utils.module.css'
+
+export interface ProgressBarProps {
+  data: string
+  percentage: string
+}
+
+
+export default function ProgressBar ({ data, percentage }: ProgressBarProps, key: string): JSX.Element {
+  return (
+    <div className='w-full flex flex-col md:flex-row flex-nowrap m-auto items-start md:items-center'>
+      <span className='inline mx-2 md:w-[20%] text-right'>{data}</span>
+      <div className='w-full md:w-[80%] flex flex-row'>
+        <div className={`w-[80%] border-opacity-10 h-3 m-2 rounded-lg ${utilStyles['inner-shadow']}`}>
+          {/* TODO fix progress bar width */}
+          <div style={{ width: `${percentage}%` }} className={`bg-[#00eeffd2] h-full rounded-lg ${utilStyles.blueShadow}`} />
+        </div>
+        <span className='w-[20%]'>{percentage}%</span>
+      </div>
+    </div>
+  )
+}
---
+++ app/ui/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,29 @@
+import ProgressBar from '../ProgressBar/ProgressBar'
+
+interface ProjectCardProps {
+  id: number
+  name: string
+  url: string
+  httpClone: string
+  sshClone: string
+  languages: { language: string, percentage: string }[]
+}
+
+export default function ProjectCard ({ name, url, languages }: ProjectCardProps, key: number): JSX.Element {
+  return (
+    <div
+      className='w-[95%] m-2 lg:w-[45%] p-4 rounded-lg lg:m-3 card-shadow scroll overflow-y-auto overflow-x-hidden h-auto md:h-56 bg-[#4948483d] lg4:hover:scale-105 hover:cursor-pointer'
+      key={key}
+      // onClick={() => { window.location.href = url }}
+    >
+      <p className='text-3xl text-ellipsis overflow-hidden whitespace-nowrap text-center'>{name}</p>
+      <div className='mt-4 mb-6'>
+        {
+          languages.map(({ language, percentage }, index) => (
+            <ProgressBar key={index} data={language} percentage={percentage} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
