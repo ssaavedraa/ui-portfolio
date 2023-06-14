72827a9b4b38ebe895afc92cd4699dc27eb6ee14
ssaavedraa <=>
2023-06-14 12:50:25 -0500

    remove onclick

--- app/ui/ProjectCard/ProjectCard.tsx
+++ app/ui/ProjectCard/ProjectCard.tsx
@@ -16,7 +16,6 @@ export default function ProjectCard ({ name, url, languages }: ProjectCardProps,
     <div
       className='w-[95%] m-2 lg:w-[45%] p-4 rounded-lg lg:m-3 card-shadow scroll overflow-y-auto overflow-x-hidden h-auto md:h-56 bg-[#4948483d] lg4:hover:scale-105 hover:cursor-pointer'
       key={key}
-      // onClick={() => { window.location.href = url }}
     >
       <p className='text-3xl text-ellipsis overflow-hidden whitespace-nowrap text-center'>{name}</p>
       <div className='mt-4 mb-6'>
