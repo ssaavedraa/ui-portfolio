27b3f43238b5db7cfe0f9c2bb6de32e2e03851b8
ssaavedraa <=>
2023-06-15 04:01:07 -0500

    fix scrol on mobile

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,9 +7,9 @@ export default async function Projects () {
   const projects = await fetchGithubProjects()
 
   return (
-    <div className='w-[95%] lg:w-[97%] m-auto overflow-x-hidden h-[85vh] mt-[15vh]'>
+    <div className='w-[95%] lg:w-[97%] m-auto overflow-y-auto h-[72vh] lg:h-[82vh] mt-[15vh] pb-2 absolute left-0 right-0'>
       <p className='text-6xl my-4 text-center'>Projects</p>
-      <div className='m-auto flex flex-col md:flex-row flex-wrap justify-center overflow-y-auto scroll lg:h-[75vh] w-full 2xl:w-[60vw]'>
+      <div className='m-auto flex flex-col md:flex-row flex-wrap justify-center h-auto w-full 2xl:w-[60vw]'>
         {projects.map(({ id, name, url, httpClone, sshClone, languages }) => (
             <ProjectCard
               id={id}
