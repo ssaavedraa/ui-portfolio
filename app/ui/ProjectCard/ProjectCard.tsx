fd6766422f14fceb763315e2941029123d82689c
ssaavedraa <=>
2023-07-15 14:29:04 -0500

    fix iframe responsive design

--- app/ui/ProjectCard/ProjectCard.tsx
+++ app/ui/ProjectCard/ProjectCard.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect } from 'react'
 import ProgressBar from '../ProgressBar/ProgressBar'
 
 interface ProjectCardProps {
@@ -14,15 +15,39 @@ interface ProjectCardProps {
 }
 
 export default function ProjectCard ({ name, url, languages, deployed, deploymentUrl }: ProjectCardProps, key: number): JSX.Element {
+  const iframeId = `${name}-iframe`
+
+  useEffect(() => {
+    if (deployed) {
+      const viewportWidth = window.innerWidth
+      const cardElement = document.getElementById(name)
+
+      const cardWidth = cardElement?.clientWidth
+
+      const iframe = document.getElementById(iframeId)
+      const iframeScale = cardWidth! / viewportWidth
+      const iframeHeight = iframe?.clientHeight
+
+      const iframeContainer = document.getElementById(`${iframeId}-container`)
+
+      iframe!.style.transform = `scale(${iframeScale})`
+      iframeContainer!.style.height = `${iframeHeight! * iframeScale}px`
+    }
+  }, [])
+
   return (
     <div
-      className='w-full overflow-clip rounded-lg card-shadow scroll overflow-y-auto h-full bg-[#4948483d] hover:cursor-pointer'
+      className='w-full overflow-clip rounded-lg card-shadow scroll overflow-y-auto h-full bg-[#4948483d] hover:cursor-pointer my-2 md:my-0'
       key={key}
+      id={name}
     >
       {deployed && (
-        <div className='w-screen h-[291px] overflow-clip rounded-b-lg'>
-          <iframe src={deploymentUrl} frameBorder="0" className='w-full aspect-video pointer-events-none scale-[44%] origin-top-left'></iframe>
-        </div>)}
+        <div className='w-full h-auto overflow-clip rounded-b-lg'>
+          <div className='w-screen h-screen' id={`${iframeId}-container`}>
+            <iframe src={deploymentUrl} className='w-full aspect-[9/16] md:aspect-square lg:aspect-video pointer-events-none origin-top-left' id={iframeId}></iframe>
+          </div>
+        </div>
+      )}
       <p className='text-3xl text-ellipsis overflow-hidden whitespace-nowrap text-center mt-4'>{name}</p>
       <div className='mt-4 m-4'>
         {
